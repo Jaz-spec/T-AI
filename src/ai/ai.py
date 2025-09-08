c932390cfc5ebb01efd1ab78fad0616f269d70ee
Jaz-spec <jaz.edna@proton.me>
2025-09-08 16:42:07 +0100

    feat: implements rudimentary tool use

--- src/ai/ai.py
+++ src/ai/ai.py
@@ -1,12 +1,15 @@
 import requests
 import click
+import subprocess
 
-def getPhi(user_input, model='phi3:mini'):
-    # This connects to your LOCAL Ollama server
+
+def getPhi(user_input, system_prompt="be helpful", model='phi3:mini'):
+    # Connects to local Ollama server
     url = "http://localhost:11434/api/generate"
     data = {
         "model": model,
         "prompt": user_input,
+        "system": system_prompt,
         "stream": False
     }
 
@@ -16,8 +19,46 @@ def getPhi(user_input, model='phi3:mini'):
 @click.command()
 @click.option('--user_input', prompt='Enter your message here')
 def main(user_input):
-    response = getPhi(user_input)
-    click.echo(f"Agent: {response}")
+    if user_input and user_input.split(' ', 1)[0].lower() == "timetrack":
+        results = subprocess.run(user_input,
+            shell=True,
+            capture_output=True,
+            text=True)
+        print(results.stdout)
+    else:
+        click.echo("Choosing tool")
+        tool = chooseTool(user_input)
+        command = getCommand(tool)
+        if command:
+            print("command present")
+            commandResults = subprocess.run(command, shell=True, capture_output=True, text=True)
+            print(commandResults.stdout)
+
+def getCommand(tool):
+    print("geting command")
+    lines = tool.split('\n')
+    for line in lines:
+        if line and line.strip().startswith("COMMAND:"):
+            arg = line.replace("COMMAND:", "")
+            print(f"ARG: {arg}")
+            return arg
 
 if __name__ == "__main__":
     main()
+
+def chooseTool(user_input):
+    system_prompt = """
+    Your job is to choose the most relevent tool out of the following list and respond in the response format with no extra text:
+       timetrack: a tool that tracks time
+       timetrack tool commands:
+           timetrack start: starts the timer
+           timetrack stop: stops the Timer
+
+
+    response format:
+        TOOL: [insert tool name here]
+        COMMAND: [insert relevent command here]
+    """
+    tool = getPhi(user_input, system_prompt)
+    print(tool)
+    return tool
--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,3 +1,3 @@
 """A minimal cli tool using click"""
 
-__version__="0.1.0"
+__version__ = "0.1.0"
